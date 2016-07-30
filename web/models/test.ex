46682993a436dd1771606c304d1788e6e22ed11e
developerworks <developerworks@163.com>
2016-07-31 00:23:05 +0800

    Modified: insert data by creating a changeset

--- web/models/test.ex
+++ web/models/test.ex
@@ -17,12 +17,9 @@ defmodule Bytea.Test do
     |> validate_required([:photo])
   end
 
-  def insert(map) do
-    Map.merge(%__MODULE__{}, map) |> Repo.insert
-  end
-
   def test_insert do
     {:ok, contents} = File.read("/tmp/FB7D22A5-924E-4DA5-AC93-5D815FA1AEA8.png")
-    insert(%{photo: contents})
+    changeset(%__MODULE__{}, %{photo: contents})
+    |> Repo.insert
   end
 end
